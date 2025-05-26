68158365ac7c59db5f4f6b81c7c9abc36fcdda20
Muaz Hussain <muaz.self@gmail.com>
2025-05-27 02:25:32 +0600

    Feat: using var keyword

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello!")
+	// variable declaration using var keyword
+	var smsSendingLimit int
+	var costPerSMS float32
+	var hasPermission bool
+	var userName string
+
+	fmt.Printf(
+		"%d %f %t %s\n",
+		smsSendingLimit,
+		costPerSMS,
+		hasPermission,
+		userName,
+	)
 }
