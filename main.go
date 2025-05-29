71c9cd9d29476a8a262643b45c78646572385a76
Muaz Hussain <muaz.self@gmail.com>
2025-05-30 02:11:06 +0600

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	s := "This is a string"
-	// String is an array
-	// These are aliases of bytes
-
-	fmt.Printf("%T %v\n", s[2], s[2])
-
-	// String are imutable
-	// This is an error
-	s[2] = "u"
+	// This is a rune
+	a := 'u'
+	fmt.Printf("%v %T\n", a, a)
 }
