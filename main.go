ca6f12d68cb504173e0cd2850b976f21e77c3e95
Muaz Hussain <muaz.self@gmail.com>
2025-05-30 02:10:17 +0600

    string examples

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello!")
+	s := "This is a string"
+	// String is an array
+	// These are aliases of bytes
+
+	fmt.Printf("%T %v\n", s[2], s[2])
+
+	// String are imutable
+	// This is an error
+	s[2] = "u"
 }
