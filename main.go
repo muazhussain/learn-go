ddf8079356e6a2aeec4e9ff689b93a0a594b73b6
Muaz Hussain <muaz.self@gmail.com>
2025-06-08 20:35:34 +0600

    slice capacity

--- main.go
+++ main.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main() {
 	a := []int{1, 2, 3}
-	fmt.Printf("slice: %v\nlength: %v\ntype: %T\n", a, len(a), a)
+	fmt.Printf("Length: %v\n", len(a))
+	fmt.Printf("Capacity: %v\n", cap(a))
+	a = append(a, 10)
+	fmt.Printf("Capacity: %v\n", cap(a))
 }
