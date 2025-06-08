afc5b972e8ce9fdc67327214b01ed24fa3e84fd0
Muaz Hussain <muaz.self@gmail.com>
2025-06-08 20:33:24 +0600

    slice

--- main.go
+++ main.go
@@ -3,10 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	a := [3]int{1, 3, 5}
-	// copy (does not reference)
-	b := &a
-	b[1] = 4
-	fmt.Println(a)
-	fmt.Println(b)
+	a := []int{1, 2, 3}
+	fmt.Printf("slice: %v\nlength: %v\ntype: %T\n", a, len(a), a)
 }
