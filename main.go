94fed5e6613d43e44e0eee2ff35c0241b8be43a7
Muaz Hussain <muaz.self@gmail.com>
2025-06-08 21:01:22 +0600

    concate slices

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
-	a := []int{}
-	a = append(a, 2) // adding new element into the slice
+	a := []int{1, 2, 3}
+	b := []int{4, 5, 6}
+	c := append(a, b...)
+	fmt.Println(c)
 }
