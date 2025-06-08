627c4305e313da93b9d821135c2cb43733e5e5de
Muaz Hussain <muaz.self@gmail.com>
2025-06-08 20:36:49 +0600

    slices are default reference type

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	a := []int{1, 2, 3}
-	fmt.Printf("Length: %v\n", len(a))
-	fmt.Printf("Capacity: %v\n", cap(a))
-	a = append(a, 10)
-	fmt.Printf("Capacity: %v\n", cap(a))
+	b := a
+	b[1] = 9
+	fmt.Println(a)
+	fmt.Println(b)
 }
