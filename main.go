90ce877ae00eff7586dc207811cde48a4475fd62
Muaz Hussain <muaz.self@gmail.com>
2025-06-15 01:28:42 +0600

    using make function

--- main.go
+++ main.go
@@ -3,16 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	statePopulation := map[string]int{
+	statePolulation := make(map[string]int)
+	statePolulation = map[string]int{
 		"Dhaka":  150000,
 		"Khulna": 2000,
 	}
-	fmt.Println(statePopulation)
-	// Array can be key
-	m := map[[3]int]int {}
-	fmt.Println(m)
-
-	// Slices can't be key
-	n := map[[]int]int {}
-	fmt.Println(n)
+	fmt.Println(statePolulation)
 }
