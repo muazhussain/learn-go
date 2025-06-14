5f87bbe8e9491fe799331ae9f82c1296966d55eb
Muaz Hussain <muaz.self@gmail.com>
2025-06-15 01:15:50 +0600

    key eligibility conditions

--- main.go
+++ main.go
@@ -8,4 +8,11 @@ func main() {
 		"Khulna": 2000,
 	}
 	fmt.Println(statePopulation)
+	// Array can be key
+	m := map[[3]int]int {}
+	fmt.Println(m)
+
+	// Slices can't be key
+	n := map[[]int]int {}
+	fmt.Println(n)
 }
