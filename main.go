1c0c19e73633eee8b68f369ba0cb94f3c4ddda63
Muaz Hussain <muaz.self@gmail.com>
2025-06-16 01:12:30 +0600

    conditional statements

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello!")
+	statePopulation := map[string]int{
+		"Dhaka":   1000,
+		"Khulna":  250,
+		"Jashore": 120,
+	}
+	if pop, ok := statePopulation["Jashore"]; ok {
+		fmt.Println(pop)
+	} else {
+		fmt.Println("Info does not exist!")
+	}
 }
