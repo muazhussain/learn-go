49473eb686570197df18003da0b6e4a16c7edc86
Muaz Hussain <muaz.self@gmail.com>
2025-06-16 01:35:31 +0600

    continue statement

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	i := 0
-	for {
-		fmt.Println(i)
-		i++
-		if i >= 5 {
-			break
+	for i := 0; i <= 10; i++ {
+		if i % 2 == 0 {
+			continue
 		}
+		fmt.Println(i)
 	}
 }
