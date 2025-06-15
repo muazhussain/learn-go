1fe4ed5e4937962a9d8fc75c3dc3a4daff4a4692
Muaz Hussain <muaz.self@gmail.com>
2025-06-16 01:22:31 +0600

    switch-case

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	statePopulation := map[string]int{
-		"Dhaka":   1000,
-		"Khulna":  250,
-		"Jashore": 120,
-	}
-	if pop, ok := statePopulation["Jashore"]; ok {
-		fmt.Println(pop)
-	} else {
-		fmt.Println("Info does not exist!")
+	i := 16
+	switch {
+	case i <= 10:
+		fmt.Println("Less than or equal to 10")
+		fallthrough
+	case i >= 15:
+		fmt.Println("Less than or equal to 15")
+		fallthrough
+	case i <= 20:
+		fmt.Println("Less than or equal to 20")
+		fallthrough
+	default:
+		fmt.Println("Unknown")
 	}
 }
