25e18769c252ef9efd11545457c681d235b91350
Muaz Hussain <muaz.self@gmail.com>
2025-06-29 21:45:46 +0600

    basic defer

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello!")
+	fmt.Println("first")
+	// defer execute at the end just before the function return
+	defer fmt.Println("second")
+	fmt.Println("third")
 }
