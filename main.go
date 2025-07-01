be09c9a1ecd0fd7ab9d771139f4832f4c2a6d1b1
Muaz Hussain <muaz.self@gmail.com>
2025-07-01 15:46:03 +0600

    goroutine

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	wg      = sync.WaitGroup{}
+	counter = 0
+	m       = sync.RWMutex{}
+)
 
 func main() {
-	fmt.Println("hello!")
+	runtime.GOMAXPROCS(100)
+	for range 10 {
+		wg.Add(2)
+		m.RLock()
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello() {
+	fmt.Printf("Hello #%v\n", counter)
+	m.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
 }
